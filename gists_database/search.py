c6e409e6bcbe90cda7e298a5f7e7220a3399a99d
Houda Belefqih <houdabelefqih@gmail.com>
2019-09-21 18:37:23 -0500

    Added search function w/ created_at and github_id filters

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,37 @@
 from .models import Gist
 
+
 def search_gists(db_connection, **kwargs):
-    pass
+
+    query = 'SELECT * FROM gists;'
+
+    if kwargs:
+        query = build_query(kwargs.items())
+
+    cursor = db_connection.execute(query)
+
+    gists = [Gist(gist) for gist in cursor.fetchall()]
+
+    return gists
+
+
+def build_query(kwargs_items):
+
+    query = 'SELECT * FROM gists WHERE'
+
+    where_clause = ''
+
+    for key, value in kwargs_items:
+        if key == 'created_at':
+            value = format_created_at(value)
+
+        where_clause += f" {key} = '{value}'"
+
+    where_clause.replace(" ", " AND ")
+
+    return query + where_clause
+
+
+def format_created_at(datetime):
+    return datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+
